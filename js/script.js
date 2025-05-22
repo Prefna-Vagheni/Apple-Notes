48c305a1bc25791b87d54eb36183e11c3644111c
Prefna Vagheni <prefnavagheni54@gmail.com>
2025-05-22 13:10:34 +0200

    Commit with HTML and CSS features

---
+++ js/script.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const notesContainer = document.querySelector('.notes-container');
+const addNote = document.querySelector('.add-note');
+const searchNote = document.querySelector('.search-bar');
+const editTheList = document.querySelector('.edit');
+const searchWithVoice = document.querySelector('.fa-microphone');
+
+document.querySelector('.year').textContent = new Date().getFullYear();
